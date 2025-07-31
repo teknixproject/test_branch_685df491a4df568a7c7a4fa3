0f4405f058357fd5368409daf524e646f95b8c88
ryannguyen <170920443+ryannguyen2000@users.noreply.github.com>
2025-07-31 16:17:47 +0700

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import EntryPage from '@/components/page/EntryPage';
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-export default async function Home() {
-  console.log('HomeHome123');
+export const hihi = 10;
 
+export default async function Home() {
   return <EntryPage />;
 }
