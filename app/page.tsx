87285e4fd7159ac7017f9aecce4b1b7c1846225c
ryannguyen <203745776+teknixproject@users.noreply.github.com>
2025-08-11 09:04:51 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default async function Home() {
-  console.log('Hihihaha');
+  console.log('Hihihaha 123');
 
   return <EntryPage />;
 }
