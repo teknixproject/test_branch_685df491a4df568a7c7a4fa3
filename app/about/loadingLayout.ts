cee0f63f4e58c7ec17f27d77e3ddbd62729c4e25
teknixproject <project@teknixcorp.com>
2025-08-18 09:47:14 +0700

    Update loadingLayout.ts

--- app/about/loadingLayout.ts
+++ app/about/loadingLayout.ts
@@ -142,6 +142,159 @@ export const loadingLayout = {
               }
             }
           }
+        },
+        {
+          "_id": "684942a5ea3ce52c55586d23",
+          "value": "Table",
+          "name": "Table",
+          "type": "DataDisplay",
+          "createdAt": "2025-06-05T10:06:01.869Z",
+          "updatedAt": "2025-06-05T10:06:01.869Z",
+          "__v": 0,
+          "id": "Table$6b58511a-b444-486f-ace1-f7f4da1883d7",
+          "childs": [],
+          "thumbnail": "",
+          "componentProps": {
+            "size": "middle",
+            "tableLayout": "auto",
+            "bordered": false,
+            "loading": false,
+            "pagination": false,
+            "pagination-current": 1,
+            "pagination-total": 10,
+            "pagination-limit": 1000,
+            "pagination-pageSizeOptions": [
+              10,
+              20,
+              50,
+              100
+            ],
+            "pagination-pageSize": 100,
+            "dataSource": [
+              {
+                "key": "1",
+                "name": "Mike",
+                "age": 32,
+                "address": "10 Downing Street"
+              },
+              {
+                "key": "2",
+                "name": "Mike",
+                "age": 32,
+                "address": "10 Downing Street"
+              },
+              {
+                "key": "3",
+                "name": "Mike",
+                "age": 32,
+                "address": "10 Downing Street"
+              },
+              {
+                "key": "4",
+                "name": "Mike",
+                "age": 32,
+                "address": "10 Downing Street"
+              }
+            ],
+            "columns": [
+              {
+                "title": "Name",
+                "dataIndex": "name",
+                "key": "name",
+                "box": {
+                  "id": "Flex$af4561d0-e44b-4439-a761-b40715782fd3",
+                  "value": "Flex",
+                  "name": "Flex",
+                  "childs": [],
+                  "type": "Layout",
+                  "componentProps": {
+                    "style": {
+                      "display": "flex",
+                      "gap": "5px"
+                    }
+                  }
+                }
+              },
+              {
+                "title": "Age",
+                "dataIndex": "age",
+                "key": "age",
+                "box": {
+                  "id": "Flex$2f340935-6e46-4391-a691-67ee36b07a74",
+                  "value": "Flex",
+                  "name": "Flex",
+                  "childs": [],
+                  "type": "Layout",
+                  "componentProps": {
+                    "style": {
+                      "display": "flex",
+                      "gap": "5px"
+                    }
+                  }
+                }
+              },
+              {
+                "title": "Address",
+                "dataIndex": "address",
+                "key": "address",
+                "box": {
+                  "id": "Flex$de4046ba-bc7a-4336-914a-ba835c84c209",
+                  "value": "Flex",
+                  "name": "Flex",
+                  "childs": [],
+                  "type": "Layout",
+                  "componentProps": {
+                    "style": {
+                      "display": "flex",
+                      "gap": "5px"
+                    }
+                  }
+                }
+              }
+            ],
+            "dataProps": [
+              {
+                "name": "pagination-onChange",
+                "label": "Paginationn OnChange",
+                "type": "MouseEventHandler",
+                "args": [
+                  {
+                    "name": "page",
+                    "index": 0,
+                    "type": "number",
+                    "isList": false
+                  },
+                  {
+                    "name": "pageSize",
+                    "index": 1,
+                    "type": "number",
+                    "isList": false
+                  }
+                ]
+              },
+              {
+                "name": "rowSelection.onChange",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onClick",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onDoubleClick",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onMouseEnter",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onMouseLeave",
+                "type": "MouseEventHandler"
+              }
+            ],
+            "footerColumns": []
+          }
         }
       ],
       "typeScreen": "pc",
