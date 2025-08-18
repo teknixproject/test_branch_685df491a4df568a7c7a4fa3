8dcfd83517234552ffaf7107b00e6974aa90bcd0
teknixproject <project@teknixcorp.com>
2025-08-18 11:44:02 +0700

    Update loadingLayout.ts

--- app/about/loadingLayout.ts
+++ app/about/loadingLayout.ts
@@ -45,97 +45,209 @@ export const loadingLayout = {
       },
       "childs": [
         {
-          "_id": "685cb64ab24a6b0da3f2b743",
-          "value": "Flex",
-          "name": "Flex",
-          "type": "Layout",
-          "createdAt": "2025-06-26T02:54:02.761Z",
-          "updatedAt": "2025-06-26T02:54:02.761Z",
+          "_id": "684942a5ea3ce52c55586d23",
+          "value": "Table",
+          "name": "Table",
+          "type": "DataDisplay",
+          "createdAt": "2025-06-05T10:06:01.869Z",
+          "updatedAt": "2025-06-05T10:06:01.869Z",
           "__v": 0,
-          "id": "Flex$79c5e240-12be-4af6-b184-46516f628137",
-          "childs": [
-            {
-              "_id": "685cb49ff24a005ec3696aa6",
-              "value": "Title",
-              "name": "Title",
-              "type": "General",
-              "createdAt": "2025-06-26T02:46:55.615Z",
-              "updatedAt": "2025-06-26T02:46:55.615Z",
-              "__v": 0,
-              "id": "Title$c1d18f48-6bfb-478c-a51d-8ee29e8baa4c",
-              "childs": [],
-              "thumbnail": "",
-              "componentProps": {
-                "children": {
-                  "valueInput": "Loading About HEHEHEHEHEHE",
-                  "type": "valueInput"
-                },
-                "level": {
-                  "valueInput": 4,
-                  "type": "valueInput"
-                },
-                "type": {
-                  "valueInput": "default",
-                  "type": "valueInput"
-                },
-                "underline": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "delete": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "strong": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "itatlic": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "code": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "disabled": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "mark": {
-                  "valueInput": false,
-                  "type": "valueInput"
-                },
-                "dataProps": [
-                  {
-                    "name": "onClick",
-                    "type": "MouseEventHandler"
-                  }
-                ],
-                "italic": {
-                  "type": "valueInput"
-                }
-              }
-            }
-          ],
+          "id": "Table$8685de39-310d-42ca-8995-00f695ac62b6",
+          "childs": [],
           "thumbnail": "",
           "componentProps": {
+            "size": {
+              "valueInput": "middle",
+              "type": "valueInput"
+            },
+            "tableLayout": {
+              "valueInput": "auto",
+              "type": "valueInput"
+            },
+            "bordered": {
+              "valueInput": false,
+              "type": "valueInput"
+            },
+            "loading": {
+              "valueInput": false,
+              "type": "valueInput"
+            },
+            "pagination": {
+              "valueInput": false,
+              "type": "valueInput"
+            },
+            "pagination-current": {
+              "valueInput": 1,
+              "type": "valueInput"
+            },
+            "pagination-total": {
+              "valueInput": 10,
+              "type": "valueInput"
+            },
+            "pagination-limit": {
+              "valueInput": 1000,
+              "type": "valueInput"
+            },
+            "pagination-pageSizeOptions": {
+              "valueInput": [
+                10,
+                20,
+                50,
+                100
+              ],
+              "type": "valueInput"
+            },
+            "pagination-pageSize": {
+              "valueInput": 100,
+              "type": "valueInput"
+            },
+            "dataSource": {
+              "valueInput": [
+                {
+                  "key": "1",
+                  "name": "Mike",
+                  "age": 32,
+                  "address": "10 Downing Street"
+                },
+                {
+                  "key": "2",
+                  "name": "Mike",
+                  "age": 32,
+                  "address": "10 Downing Street"
+                },
+                {
+                  "key": "3",
+                  "name": "Mike",
+                  "age": 32,
+                  "address": "10 Downing Street"
+                },
+                {
+                  "key": "4",
+                  "name": "Mike",
+                  "age": 32,
+                  "address": "10 Downing Street"
+                }
+              ],
+              "type": "valueInput"
+            },
+            "columns": [
+              {
+                "title": "Name",
+                "dataIndex": "name",
+                "key": "name",
+                "box": {
+                  "id": "Flex$504db331-1a5d-4f5b-b7c2-ea04638206e6",
+                  "value": "Flex",
+                  "name": "Flex",
+                  "childs": [],
+                  "type": "Layout",
+                  "componentProps": {
+                    "style": {
+                      "display": "flex",
+                      "gap": "5px"
+                    }
+                  }
+                }
+              },
+              {
+                "title": "Age",
+                "dataIndex": "age",
+                "key": "age",
+                "box": {
+                  "id": "Flex$3c232327-c48d-46b7-98b4-f8d68baafbfe",
+                  "value": "Flex",
+                  "name": "Flex",
+                  "childs": [],
+                  "type": "Layout",
+                  "componentProps": {
+                    "style": {
+                      "display": "flex",
+                      "gap": "5px"
+                    }
+                  }
+                }
+              },
+              {
+                "title": "Address",
+                "dataIndex": "address",
+                "key": "address",
+                "box": {
+                  "id": "Flex$b75612bd-2a16-434f-93ab-47c6e2186536",
+                  "value": "Flex",
+                  "name": "Flex",
+                  "childs": [],
+                  "type": "Layout",
+                  "componentProps": {
+                    "style": {
+                      "display": "flex",
+                      "gap": "5px"
+                    }
+                  }
+                }
+              }
+            ],
             "dataProps": [
               {
-                "name": "onClick",
+                "name": "pagination-onChange",
+                "label": "Paginationn OnChange",
+                "type": "MouseEventHandler",
+                "args": [
+                  {
+                    "name": "page",
+                    "index": 0,
+                    "type": "number",
+                    "isList": false
+                  },
+                  {
+                    "name": "pageSize",
+                    "index": 1,
+                    "type": "number",
+                    "isList": false
+                  }
+                ]
+              },
+              {
+                "name": "rowSelection.onChange",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onClick",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onDoubleClick",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onMouseEnter",
+                "type": "MouseEventHandler"
+              },
+              {
+                "name": "onRow.onMouseLeave",
                 "type": "MouseEventHandler"
               }
             ],
             "styleMultiple": {
               "normal": {
-                "display": "flex",
-                "flexDirection": "row",
-                "paddingTop": "5px",
-                "paddingLeft": "5px",
-                "paddingRight": "5px",
-                "paddingBottom": "5px"
+                "width": "100%"
               }
-            }
+            },
+            "enableRowSelection": {
+              "type": "valueInput"
+            },
+            "enableFooter": {
+              "type": "valueInput"
+            },
+            "rowSelectionType": {
+              "type": "valueInput"
+            },
+            "rowSelectionFixed": {
+              "type": "valueInput"
+            },
+            "rowSelectionPreserveSelectedRowKeys": {
+              "type": "valueInput"
+            },
+            "footerColumns": []
           }
         }
       ],
