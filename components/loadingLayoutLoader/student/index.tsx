4acd2ab2b95266544b02c955dcd7e689ccd30659
teknixproject <project@teknixcorp.com>
2025-08-19 15:06:39 +0700

    Update index.tsx

--- components/loadingLayoutLoader/student/index.tsx
+++ components/loadingLayoutLoader/student/index.tsx
@@ -8,7 +8,7 @@ export default function LoadingStudent() {
       <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
       <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
       <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
-      <Flex style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","width":"100%","gap":"5px"}}>
+      <Flex style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","width":"100%","gap":"5px","justifyContent":"center","alignItems":"center"}}>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
