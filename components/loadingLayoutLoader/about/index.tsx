533062597ffe656702c0e322ac73ca6738a5cede
teknixproject <project@teknixcorp.com>
2025-08-19 14:35:41 +0700

    Update index.tsx

--- components/loadingLayoutLoader/about/index.tsx
+++ components/loadingLayoutLoader/about/index.tsx
@@ -4,13 +4,13 @@ import { Flex, Skeleton } from 'antd';
 export default function LoadingAbout() {
   return (
     <div className="relative !z-0 h-screen">
-      <Flex style={{ "display": "flex", "flexDirection": "row", "paddingTop": "5px", "paddingLeft": "5px", "paddingRight": "5px", "paddingBottom": "5px", "height": "100%", "justifyContent": "center", "alignItems": "center" }}>
-        <Skeleton active={true} avatar={true} title={true} paragraph={true}></Skeleton>
-        <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
-        <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
-        <Skeleton.Input active={true} avatar={true} title={true} paragraph={true}></Skeleton.Input>
-      </Flex>
+    <Flex style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","height":"100%","justifyContent":"center","alignItems":"center","gap":"20px"}}>
+      <Skeleton active={true} avatar={true} title={true} paragraph={true}></Skeleton>
+      <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
+      <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+      <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
+      <Skeleton.Input active={true} avatar={true} title={true} paragraph={true}></Skeleton.Input>
+    </Flex>
     </div>
   );
 }
