6108bc1f334be3bcb2ed47a2310244a7f620ac97
teknixproject <project@teknixcorp.com>
2025-08-19 16:30:02 +0700

    Update index.tsx

--- components/loadingLayoutLoader/student/index.tsx
+++ components/loadingLayoutLoader/student/index.tsx
@@ -4,16 +4,11 @@ import { Flex, Skeleton } from 'antd';
 export default function LoadingStudent() {
   return (
     <div className="relative !z-0 h-screen">
-    <Flex style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","height":"100%","gap":"10px","justifyContent":"center","alignItems":"center"}}>
+    <Flex style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","height":"100%","gap":"10px"}}>
       <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
+      <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
       <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
-      <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
-      <Flex style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","width":"100%","gap":"5px","justifyContent":"center","alignItems":"center"}}>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
-      </Flex>
+      <Skeleton.Input active={true} avatar={true} title={true} paragraph={true}></Skeleton.Input>
     </Flex>
     </div>
   );
