45fb4dd8a18032e490a2c293a09669127d8bb5fe
teknixproject <project@teknixcorp.com>
2025-08-19 14:55:56 +0700

    Create index.tsx

---
+++ components/loadingLayoutLoader/student/index.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { Flex, Skeleton } from 'antd';
+
+export default function LoadingStudent() {
+  return (
+    <div className="relative !z-0 h-screen">
+    <Flex style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","height":"100%","gap":"10px","justifyContent":"center","alignItems":"center"}}>
+      <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
+      <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
+      <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
+      <Flex style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","width":"100%","gap":"5px"}}>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+      </Flex>
+    </Flex>
+    </div>
+  );
+}
