47681004d5ae69bf248bc74478a691d76e9a8bba
ryannguyen <203745776+teknixproject@users.noreply.github.com>
2025-08-19 11:27:57 +0700

    fix

--- utils/loadingLayoutLoader/index.tsx
+++ utils/loadingLayoutLoader/index.tsx
@@ -1,24 +1,11 @@
 'use client';
-  import {Skeleton, Modal, Flex } from 'antd';
-import LoadingHome from "./loadingHome";
 
-  export default function LoadingRoot() {
-    return (
-         <flex  style={{"display":"flex","flexDirection":"column","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","width":"100%","height":"100%"}}>
- <Skeleton.undefined active={true} avatar={true} title={true} paragraph={true} style={{"width":"100%","height":"500px"}}></Skeleton.undefined>
- <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true} ></Skeleton.Avatar>
- <Skeleton.Input active={true} avatar={true} title={true} paragraph={true} ></Skeleton.Input>
- <Skeleton.undefined active={true} avatar={true} title={true} paragraph={true} ></Skeleton.undefined>
- <Skeleton.Image active={true} avatar={true} title={true} paragraph={true} ></Skeleton.Image>
- <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} ></Skeleton.Button>
- <Modal  >
- <Flex  ></Flex>
- </Modal>
- </flex>
-    )
-  }
-  
+const component: any = {}
 
-const component: any = {
-  "/": LoadingHome
-};
+const LoadingLayoutComponent = ({ pathname }: { pathname: string }) => {
+  const Component = component[pathname]
+
+  return Component ? <Component /> : <div>Loading...</div>
+}
+
+export default LoadingLayoutComponent;
