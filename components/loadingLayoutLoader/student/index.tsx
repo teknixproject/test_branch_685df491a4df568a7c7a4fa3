06676b1ac3e86b920c1e325ba52d9fb531265f54
teknixproject <project@teknixcorp.com>
2025-08-20 17:55:47 +0700

    Update index.tsx

--- components/loadingLayoutLoader/student/index.tsx
+++ components/loadingLayoutLoader/student/index.tsx
@@ -6,24 +6,50 @@ export default function LoadingStudent() {
   return (
     <div className="relative !z-0 h-screen">
     <div mount={true} style={{"display":"block","paddingTop":"10px","paddingRight":"10px","paddingBottom":"10px","paddingLeft":"10px","width":"100%","height":"100%","justifyContent":"center","alignItems":"center","gap":"10px"}}>
-      <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Avatar>
-      <Skeleton.Image active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Image>
-      <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Button>
-      <Skeleton.Input active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Input>
+      <div style={{"display":"block"}}>
+      <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
+      </div>
+      <div style={{"display":"block"}}>
+      <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
+      </div>
+      <div style={{"display":"block"}}>
+      <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+      </div>
+      <div style={{"display":"block"}}>
+      <Skeleton.Input active={true} avatar={true} title={true} paragraph={true}></Skeleton.Input>
+      </div>
       <div style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
-        <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Avatar>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Button>
+        <div style={{"display":"block"}}>
+        <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
+        </div>
+        <div style={{"display":"block"}}>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        </div>
       </div>
       <div style={{"display":"grid","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Button>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Button>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Button>
+        <div style={{"display":"block"}}>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        </div>
+        <div style={{"display":"block"}}>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        </div>
+        <div style={{"display":"block"}}>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        </div>
       </div>
       <div style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","justifyContent":"center","alignItems":"center","gap":"10px"}}>
-        <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Avatar>
-        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Button>
-        <Skeleton.Image active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Image>
-        <Skeleton.Input active={true} avatar={true} title={true} paragraph={true} style={{"display":"block"}}></Skeleton.Input>
+        <div style={{"display":"block"}}>
+        <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
+        </div>
+        <div style={{"display":"block"}}>
+        <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
+        </div>
+        <div style={{"display":"block"}}>
+        <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
+        </div>
+        <div style={{"display":"block"}}>
+        <Skeleton.Input active={true} avatar={true} title={true} paragraph={true}></Skeleton.Input>
+        </div>
       </div>
     </div>
     </div>
