1d09b080c985349bb7832782c4c0850cc1abd746
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-08-20 16:06:02 +0700

    fix

--- components/loadingLayoutLoader/index.tsx
+++ components/loadingLayoutLoader/index.tsx
@@ -1,15 +1,8 @@
 'use client';
 
 import LoadingDefaultXstudio from "./LoadingDefaultXstudio";
-import LoadingRoot from "./loadingRoot";
-import LoadingAbout from "./about";
-import LoadingStudent from "./student";
 
-const component: any = {
-  "/": LoadingRoot,
-  "/about": LoadingAbout,
-  "/student": LoadingStudent
-};
+const component: any = {};
 
 const LoadingLayoutComponent = ({ pathname }: { pathname: string }) => {
   const Component = component[pathname]
