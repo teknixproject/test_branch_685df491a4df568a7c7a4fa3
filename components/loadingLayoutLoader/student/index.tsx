620de92ad72c417552cfcaaff8dadb1c65a2c84b
teknixproject <project@teknixcorp.com>
2025-08-20 17:44:20 +0700

    Update index.tsx

--- components/loadingLayoutLoader/student/index.tsx
+++ components/loadingLayoutLoader/student/index.tsx
@@ -10,16 +10,16 @@ export default function LoadingStudent() {
       <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
       <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
       <Skeleton.Input active={true} avatar={true} title={true} paragraph={true}></Skeleton.Input>
-      <div style={{"display":"block","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
+      <div style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
         <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
       </div>
-      <div style={{"display":"block","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
+      <div style={{"display":"grid","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
       </div>
-      <div style={{"display":"block","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","justifyContent":"center","alignItems":"center","gap":"10px"}}>
+      <div style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px","justifyContent":"center","alignItems":"center","gap":"10px"}}>
         <Skeleton.Avatar active={true} avatar={true} title={true} paragraph={true}></Skeleton.Avatar>
         <Skeleton.Button active={true} avatar={true} title={true} paragraph={true}></Skeleton.Button>
         <Skeleton.Image active={true} avatar={true} title={true} paragraph={true}></Skeleton.Image>
