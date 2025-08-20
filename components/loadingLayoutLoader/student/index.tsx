e94ed3aabe9a407023a35e0fe2b301e9a9f5b365
teknixproject <project@teknixcorp.com>
2025-08-20 16:52:55 +0700

    Update index.tsx

--- components/loadingLayoutLoader/student/index.tsx
+++ components/loadingLayoutLoader/student/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Col, Flex, Row, Skeleton } from 'antd';
+import { Divider, Skeleton } from 'antd';
 
 export default function LoadingStudent() {
   return (
