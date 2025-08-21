d7fb7be1699fe760d467a027ecaf7ffae6f86b02
teknixproject <project@teknixcorp.com>
2025-08-21 13:51:45 +0700

    Update loadingRoot.tsx

--- components/loadingLayoutLoader/loadingRoot.tsx
+++ components/loadingLayoutLoader/loadingRoot.tsx
@@ -1,28 +1,28 @@
 'use client';
 import React from 'react';
-import { Skeleton } from 'antd';
+import { Flex, Skeleton } from 'antd';
 
 export default function LoadingRoot() {
   return (
     <div className="relative !z-0 h-screen">
     <div mount={true} style={{"display":"block","paddingTop":"10px","paddingRight":"10px","paddingBottom":"50px","paddingLeft":"10px","width":"100%","height":"auto"}}>
-      <Skeleton.Node size={"middle"} tableLayout={"auto"} bordered={false} loading={false} pagination={false} dataSource={[{"key":"1","name":"Mike","age":32,"address":"10 Downing Street"},{"key":"2","name":"Mike","age":32,"address":"10 Downing Street"},{"key":"3","name":"Mike","age":32,"address":"10 Downing Street"},{"key":"4","name":"Mike","age":32,"address":"10 Downing Street"}]} pagination-showSizeChanger={false}></Skeleton.Node>
-      <Skeleton.Input rootClassName="!padding-5px !display-flex !flex-direction-column !gap-15px">
-        <Skeleton.Input dataSource={[{"name":"label","type":"data"}]} label={"Email"} required={false} layout={"vertical"} block={true} rootClassName="!grid-column-span 6 / span 6">
-          <Skeleton.Input placeholder={"Nhập email của bạn"} defaultValue={""}></Skeleton.Input>
+      <Skeleton active={true} />
+      <Skeleton.Input active={true} rootClassName="!p-[5px] !flex !flex-col">
+        <Skeleton.Input active={true} block={true}>
+          <Skeleton.Input active={true} />
         </Skeleton.Input>
-        <Skeleton.Button size={"middle"} type={"primary"} iconData={null} shape={"default"} block={false} ghost={false} danger={false} disabled={false} htmlType={"submit"}></Skeleton.Button>
+        <div size={"middle"} type={"primary"} iconData={null} shape={"default"} children={"Call"} block={false} ghost={false} danger={false} disabled={false} htmlType={"submit"} />
       </Skeleton.Input>
-      <Skeleton.Image data={[{"year":"1991","value":8},{"year":"1992","value":9},{"year":"1993","value":9.1},{"year":"1994","value":9.3},{"year":"1995","value":12},{"year":"1996","value":12.9},{"year":"1997","value":12.9}]} xField={"year"} yField={"value"} title={"Title chart"} style-lineWidth={2} area-style-fill={"transparent"}></Skeleton.Image>
-      <Skeleton.Button size={"middle"} type={"primary"} shape={"default"} block={false} ghost={false} danger={false} disabled={false} htmlType={"button"}></Skeleton.Button>
-      <Skeleton.Image data={[{"year":"1991","value":8},{"year":"1992","value":9},{"year":"1993","value":9.1},{"year":"1994","value":9.3},{"year":"1995","value":12},{"year":"1996","value":12.9},{"year":"1997","value":12.9}]} xField={"year"} yField={"value"} title={"Title chart"}></Skeleton.Image>
-      <div style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
-        <Skeleton.Button></Skeleton.Button>
-        <Skeleton.Button></Skeleton.Button>
-        <Skeleton.Button size={"middle"} type={"primary"} shape={"default"} block={false} ghost={false} danger={false} disabled={false} htmlType={"button"}></Skeleton.Button>
-      </div>
-      <div style={{"display":"grid","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}></div>
-      <Skeleton.Node></Skeleton.Node>
+      <Skeleton active={true} />
+      <div size={"middle"} type={"primary"} shape={"default"} children={""} block={false} ghost={false} danger={false} disabled={false} htmlType={"button"} />
+      <Skeleton active={true} />
+      <Flex style={{"display":"flex","flexDirection":"row","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}}>
+        <div children={"ssssssssssssss"} />
+        <div children={"aaaaaaaaa"} />
+        <div size={"middle"} type={"primary"} shape={"default"} children={""} block={false} ghost={false} danger={false} disabled={false} htmlType={"button"} />
+      </Flex>
+      <div style={{"display":"grid","paddingTop":"5px","paddingLeft":"5px","paddingRight":"5px","paddingBottom":"5px"}} />
+      <Skeleton active={true} />
     </div>
     </div>
   );
