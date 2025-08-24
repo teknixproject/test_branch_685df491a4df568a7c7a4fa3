a567122066b51e1a8c54b919729f6f958d4eabc6
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-08-24 15:32:52 +0700

    test update

--- components/loadingLayoutLoader/index.tsx
+++ components/loadingLayoutLoader/index.tsx
@@ -3,12 +3,14 @@
 import LoadingRemove from "./LoadingRemove"
 import LoadingDefaultXstudio from "./LoadingDefaultXstudio";
 
-const component: any = {}
+const component: any = {
+}
 
 const LoadingLayoutComponent = ({ pathname }: { pathname: string }) => {
   const Component = component[pathname]
 
-  return Component ? <Component /> : <LoadingDefaultXstudio />
+  // return Component ? <Component /> : <LoadingDefaultXstudio />
+  return Component ? <Component /> : <div className="">Loading...</div>
 }
 
 export default LoadingLayoutComponent;
