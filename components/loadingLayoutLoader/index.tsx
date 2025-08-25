797a4fd0153ff6baac47381209451b6a19721892
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-08-25 13:46:48 +0700

    Update index.tsx

--- components/loadingLayoutLoader/index.tsx
+++ components/loadingLayoutLoader/index.tsx
@@ -1,22 +1,47 @@
-
 'use client';
 
-import LoadingAbout from "./about";
 import LoadingDefaultXstudio from "./LoadingDefaultXstudio";
-import LoadingRemove from "./LoadingRemove"
+import LoadingRemove from "./LoadingRemove";
 import LoadingStudent from "./student";
+import LoadingAbout from "./about";
 import LoadingSubject from "./subject";
 
-const component: any = {
-  "/about": LoadingAbout,
-  "/": LoadingRemove,
-  "/student": LoadingStudent,
-  "/subject": LoadingSubject
-};
-
 const LoadingLayoutComponent = ({ pathname }: { pathname: string }) => {
-  const Component = component[pathname];
-  return Component ? <Component /> : <LoadingDefaultXstudio />;
+
+  const findMatchingRoute = (pathname: string) => {
+
+    const normalizedPath = pathname === '/' ? '/' : pathname.replace(/\/$/, '')
+
+    const routes: any = [
+      { pattern: '/', component: 'LoadingRemove' },
+      { pattern: '/about', component: 'LoadingAbout' },
+      { pattern: '/student', component: 'LoadingStudent' },
+      { pattern: '/subject', component: 'LoadingSubject' },
+    ]
+
+    for (const route of routes) {
+      if (route.pattern === normalizedPath) {
+        return route.component;
+      }
+    }
+
+    const segments = normalizedPath.split('/').filter(Boolean);
+
+    return 'LoadingDefaultXstudio';
+  }
+
+  const componentMap: Record<string, React.ComponentType> = {
+    "/about": LoadingAbout,
+    "/": LoadingRemove,
+    "/student": LoadingStudent,
+    "/subject": LoadingSubject
+  };
+
+  const componentName = findMatchingRoute(pathname);
+
+  const Component = componentMap[componentName] || LoadingDefaultXstudio
+
+  return <Component />
 };
 
 export default LoadingLayoutComponent;
