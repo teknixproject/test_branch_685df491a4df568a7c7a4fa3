6936dee40772f3bc0ff6c647564758cba4197aca
teknixproject <project@teknixcorp.com>
2025-08-25 16:04:18 +0700

    Create index.tsx

---
+++ components/loadingLayoutLoader/aboutid/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+import React from 'react';
+import { Skeleton } from 'antd';
+
+export default function LoadingAboutId() {
+  return (
+    <div className="relative !z-0 h-screen">
+    <div style={{"display":"block","paddingTop":"10px","paddingRight":"10px","paddingBottom":"10px","paddingLeft":"10px","width":"100%","height":"100%"}}>
+      <Skeleton active={true} block={true} />
+      <Skeleton active={true} block={true} />
+    </div>
+    </div>
+  );
+}
