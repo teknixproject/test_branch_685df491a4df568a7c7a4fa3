2f1fe0ebae5749edf9ae20012c3cfe3c0052888b
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-08-29 11:27:11 +0700

    clean

--- components/customComponents/index.tsx
+++ components/customComponents/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import PreviewOne.tsx from './PreviewOne.tsx'
-import PreviewOne from './PreviewOne'
 import LogOutSSO from './LogOutSSO'
 import LoginSSO from './LoginSSO'
 
 const CustomComponent = ({ componentName }: { componentName: string }) => {
-    const components: any = {'LoginSSO': LoginSSO,
-    'LogOutSSO': LogOutSSO,
-    'PreviewOne': PreviewOne,
-    'PreviewOne.tsx': PreviewOne.tsx
-}
+    const components: any = {
+        'LoginSSO': LoginSSO,
+        'LogOutSSO': LogOutSSO,
+    }
 
     const Component = components[componentName as keyof typeof components]
 
