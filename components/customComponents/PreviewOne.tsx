c53cd4038d4549c597b0946febc3911ee1f26aa0
teknixproject <project@teknixcorp.com>
2025-08-29 11:34:18 +0700

    Update file: PreviewOne.tsx

---
+++ components/customComponents/PreviewOne.tsx
@@ -0,0 +1,30 @@
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
+import _ from "lodash";
+import { CSSProperties } from "react";
+
+interface PreviewOneProps {
+  data?: any;
+  style?: CSSProperties;
+}
+
+const PreviewOne = ({ data, style }: PreviewOneProps) => {
+  const title = _.get(data, "title", "Title Header");
+
+  const newStyle: CSSProperties = {
+    ...style,
+  };
+
+  return (
+    <div
+      style={newStyle}
+      className="text-[#858585]"
+    >
+      {title}
+    </div>
+  );
+};
+
+export default PreviewOne;
