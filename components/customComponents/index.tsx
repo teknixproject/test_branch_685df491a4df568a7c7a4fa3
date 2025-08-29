6f068946a8ca9f9d071ff02474bcf6156d7690dc
teknixproject <project@teknixcorp.com>
2025-08-29 10:58:08 +0700

    Update index - Add PreviewOne.tsx

--- components/customComponents/index.tsx
+++ components/customComponents/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PreviewOne.tsx from './PreviewOne.tsx'
 import PreviewOne from './PreviewOne'
 import LogOutSSO from './LogOutSSO'
 import LoginSSO from './LoginSSO'
@@ -6,7 +7,8 @@ import LoginSSO from './LoginSSO'
 const CustomComponent = ({ componentName }: { componentName: string }) => {
     const components: any = {'LoginSSO': LoginSSO,
     'LogOutSSO': LogOutSSO,
-    'PreviewOne': PreviewOne
+    'PreviewOne': PreviewOne,
+    'PreviewOne.tsx': PreviewOne.tsx
 }
 
     const Component = components[componentName as keyof typeof components]
