2db6b3f2e5f998607fbf0c6c8dbf98366470fb1e
khoi_2212 <kkhoi600@gmail.com>
2025-09-11 09:18:36 +0700

    fix icon

---
+++ app/scan/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import EntryPage from '@/components/page/EntryPage';
+
+type Props = object;
+
+const Page: React.FC<Props> = ({}) => {
+  return <EntryPage></EntryPage>;
+};
+
+export default Page;
