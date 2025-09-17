9dc4d086dc489143b8970d1e122e18244f9f09d6
teknixproject <project@teknixcorp.com>
2025-09-17 14:21:09 +0700

    Update index - Add PreviewTwo

--- components/customComponents/index.tsx
+++ components/customComponents/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import PreviewTwo from './PreviewTwo'
 
 const CustomComponent = ({ componentName }: { componentName: string }) => {
-    const components: any = {}
+    const components: any = {
+    'PreviewTwo': PreviewTwo
+}
 
     const Component = components[componentName as keyof typeof components]
 
