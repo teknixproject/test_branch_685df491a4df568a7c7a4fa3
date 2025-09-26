02b7ca771236eefbf0228fd6b3d3a2a5d23c2d24
teknixproject <project@teknixcorp.com>
2025-09-26 17:17:15 +0700

    Update index - Remove Bao2

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,9 +1,6 @@
 import React from 'react'
-import Bao2 from './Bao2'
 import Bao1 from './Bao1'
 import CreateCpn2 from './CreateCpn2'
 
 export const components: any = {'CreateCpn2': CreateCpn2,
-    'Bao1': Bao1,
-    'Bao2': Bao2
-};
+    'Bao1': Bao1};
