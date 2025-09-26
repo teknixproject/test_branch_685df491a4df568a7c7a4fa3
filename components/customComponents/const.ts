836d7cb8b384278d0441015acee8471c296619d9
teknixproject <project@teknixcorp.com>
2025-09-26 15:38:54 +0700

    Update index - Remove TestCreateComponent4

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,9 +1,6 @@
 import React from 'react'
-import TestCreateComponent4 from './TestCreateComponent4'
 import TextCreateComponent3 from './TextCreateComponent3'
 import TestCreateComponent2 from './TestCreateComponent2'
 
 export const components: any = {'TestCreateComponent2': TestCreateComponent2,
-    'TextCreateComponent3': TextCreateComponent3,
-    'TestCreateComponent4': TestCreateComponent4
-};
+    'TextCreateComponent3': TextCreateComponent3};
