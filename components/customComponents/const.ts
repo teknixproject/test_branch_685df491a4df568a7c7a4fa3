6e4727d7f61bceb14e6eaf40373679ba0bd67c95
teknixproject <project@teknixcorp.com>
2025-09-26 15:32:43 +0700

    Update index - Add TestCreateComponent2

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,6 +1,7 @@
 import React from 'react'
+import TestCreateComponent2 from './TestCreateComponent2'
 import TestCreateComponent from './TestCreateComponent'
 
-export const components: any = {
-    'TestCreateComponent': TestCreateComponent
+export const components: any = {'TestCreateComponent': TestCreateComponent,
+    'TestCreateComponent2': TestCreateComponent2
 };
