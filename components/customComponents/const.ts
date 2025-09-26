56d71d5b3d911224690c2e8b13361a62724e3273
teknixproject <project@teknixcorp.com>
2025-09-26 17:25:49 +0700

    Update index - Add Bao4

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,4 +1,5 @@
 import React from 'react'
+import Bao4 from './Bao4'
 import Bao3 from './Bao3'
 import Bao22 from './Bao22'
 import Bao1 from './Bao1'
@@ -7,5 +8,6 @@ import CreateCpn2 from './CreateCpn2'
 export const components: any = {'CreateCpn2': CreateCpn2,
     'Bao1': Bao1,
     'Bao22': Bao22,
-    'Bao3': Bao3
+    'Bao3': Bao3,
+    'Bao4': Bao4
 };
