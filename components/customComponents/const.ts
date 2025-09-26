d088b9fc4a7b34a7f2511c9326cd976bbbdc3f27
teknixproject <project@teknixcorp.com>
2025-09-26 15:33:25 +0700

    Update index - Add TextCreateComponent3

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,7 +1,9 @@
 import React from 'react'
+import TextCreateComponent3 from './TextCreateComponent3'
 import TestCreateComponent2 from './TestCreateComponent2'
 import TestCreateComponent from './TestCreateComponent'
 
 export const components: any = {'TestCreateComponent': TestCreateComponent,
-    'TestCreateComponent2': TestCreateComponent2
+    'TestCreateComponent2': TestCreateComponent2,
+    'TextCreateComponent3': TextCreateComponent3
 };
