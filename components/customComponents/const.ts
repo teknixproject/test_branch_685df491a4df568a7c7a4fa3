fecb00bef8bde5cf17e5ec369dc79986ffbde4df
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-10-01 14:25:22 +0700

    Update const.ts

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,3 +1,5 @@
-import React from 'react'
-import EcommerceMetricCard from './EcommerceMetricCard'
-export const components: any = {};
+import React from 'react';
+import EcommerceMetricCard from './EcommerceMetricCard';
+export const components: any = {
+  EcommerceMetricCard: EcommerceMetricCard,
+};
