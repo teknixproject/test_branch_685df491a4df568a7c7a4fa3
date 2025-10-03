55e77dd1e09e845d20fbe17b55235e89f8ce5e25
teknixproject <project@teknixcorp.com>
2025-10-03 15:31:00 +0700

    Update index - Add SupportMetricsTailAdmin

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,6 +1,7 @@
 import React from 'react'
+import SupportMetricsTailAdmin from './SupportMetricsTailAdmin'
 import TicketDetailsTailAdmin from './TicketDetailsTailAdmin'
 
-export const components: any = {
-  'TicketDetailsTailAdmin': TicketDetailsTailAdmin
+export const components: any = {'TicketDetailsTailAdmin': TicketDetailsTailAdmin,
+    'SupportMetricsTailAdmin': SupportMetricsTailAdmin
 };
