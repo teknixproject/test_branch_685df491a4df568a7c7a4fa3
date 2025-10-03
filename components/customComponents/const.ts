e40b81281cf4cff6695cecd1d0a396224157206d
teknixproject <project@teknixcorp.com>
2025-10-03 15:44:24 +0700

    Update index - Add IntegrationCardTailAdmin

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,7 +1,9 @@
 import React from 'react'
+import IntegrationCardTailAdmin from './IntegrationCardTailAdmin'
 import SupportMetricsTailAdmin from './SupportMetricsTailAdmin'
 import TicketDetailsTailAdmin from './TicketDetailsTailAdmin'
 
 export const components: any = {'TicketDetailsTailAdmin': TicketDetailsTailAdmin,
-    'SupportMetricsTailAdmin': SupportMetricsTailAdmin
+    'SupportMetricsTailAdmin': SupportMetricsTailAdmin,
+    'IntegrationCardTailAdmin': IntegrationCardTailAdmin
 };
