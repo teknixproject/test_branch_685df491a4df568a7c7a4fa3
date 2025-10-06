082853bb8e70806e6ad099c91b18f5dd9f98b57d
teknixproject <project@teknixcorp.com>
2025-10-06 11:37:46 +0700

    Update index - Add TestCpn

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,9 +1,11 @@
 import React from 'react'
+import TestCpn from './TestCpn'
 import IntegrationCardTailAdmin from './IntegrationCardTailAdmin'
 import SupportMetricsTailAdmin from './SupportMetricsTailAdmin'
 import TicketDetailsTailAdmin from './TicketDetailsTailAdmin'
 
 export const components: any = {'TicketDetailsTailAdmin': TicketDetailsTailAdmin,
     'SupportMetricsTailAdmin': SupportMetricsTailAdmin,
-    'IntegrationCardTailAdmin': IntegrationCardTailAdmin
+    'IntegrationCardTailAdmin': IntegrationCardTailAdmin,
+    'TestCpn': TestCpn
 };
