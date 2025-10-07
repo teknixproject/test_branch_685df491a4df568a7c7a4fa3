e00c66d9bf2c6052a3de84cd3dc390aa9c27b4c5
teknixproject <project@teknixcorp.com>
2025-10-07 15:56:34 +0700

    Update index - Add PaymentMethod

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,1 +1,6 @@
-export const components: any = {};
+import React from 'react'
+import PaymentMethod from './PaymentMethod'
+
+export const components: any = {
+    'PaymentMethod': PaymentMethod
+};
