4e1e40471873c27bea444f69321daa294cf21b69
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-10-07 16:04:07 +0700

    Update const.ts

--- components/customComponents/const.ts
+++ components/customComponents/const.ts
@@ -1,1 +1,13 @@
-export const components: any = {};
+import PaymentMethod from './PaymentMethod'
+
+import TestCpn from './TestCpn'
+import IntegrationCardTailAdmin from './IntegrationCardTailAdmin'
+import SupportMetricsTailAdmin from './SupportMetricsTailAdmin'
+import TicketDetailsTailAdmin from './TicketDetailsTailAdmin'
+
+export const components: any = {'TicketDetailsTailAdmin': TicketDetailsTailAdmin,
+    'SupportMetricsTailAdmin': SupportMetricsTailAdmin,
+    'IntegrationCardTailAdmin': IntegrationCardTailAdmin,
+    'TestCpn': TestCpn,
+    'PaymentMethod': PaymentMethod
+};
