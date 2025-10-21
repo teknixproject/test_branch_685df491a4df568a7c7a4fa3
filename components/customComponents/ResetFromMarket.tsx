c9e05cdec748629d4b99268cd11775f9532a5fd6
teknixproject <project@teknixcorp.com>
2025-10-21 11:09:03 +0700

    Update file: ResetFromMarket.tsx

--- components/customComponents/ResetFromMarket.tsx
+++ components/customComponents/ResetFromMarket.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import Label from '../../tailAdminUI/components/form/Label';
+import Label from '../tailAdminUI/components/form/Label';
 import Input from '../tailAdminUI/components/form/input/InputField';
 
 const x = 5550;
